7e83b09b11b6898597920291bb6269c1a7b39506
arsonite <guenaydin.burak@gmail.com>
2021-01-23 17:14:25 +0100

    refactoring

--- _OLD/java/_new/debug/test/AppearanceTest.java
+++
@@ -1,9 +0,0 @@
-package debug.test;
-
-import game.asset.core.Appearance;
-
-public class AppearanceTest {
-    public static void main(String[] args) {
-        Appearance a = new Appearance();
-    }
-}
